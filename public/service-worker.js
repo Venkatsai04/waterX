0e107b2cdf9e75e9652b0021e9cae0efb0f1c0b2
venkat sai <saik87630@gmail.com>
2024-05-01 11:55:36 +0530

    fix

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,18 +1,25 @@
-self.addEventListener('install', (event) => {
-    event.waitUntil(
-      caches.open('my-site-cache').then((cache) => {
-        // Cache necessary resources for offline access
-        return cache.addAll([
-          '/',
+// self.addEventListener('install', (event) => {
+//     event.waitUntil(
+//       caches.open('my-site-cache').then((cache) => {
+//         // Cache necessary resources for offline access
+//         return cache.addAll([
+//           '/',
           
-          // Include other static assets (CSS, JS, images) as needed
-        ]);
-      })
-    );
-  });
+//           // Include other static assets (CSS, JS, images) as needed
+//         ]);
+//       })
+//     );
+//   });
   
-  self.addEventListener('fetch', (event) => {
-    // Handle requests for cached resources or fallback to network
-    // (Consider network-first or cache-first strategies)
-  });
+//   self.addEventListener('fetch', (event) => {
+//     // Handle requests for cached resources or fallback to network
+//     // (Consider network-first or cache-first strategies)
+//   });
   
+
+self.addEventListener('push', ()=>{
+  self.ServiceWorkerRegistration.sendNotification('test',{
+    title: 'New Notification 1',
+    body: 'This is a push notification!',
+  })
+})
